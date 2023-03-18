70650c44f6d665ad147d23c805fff04827bbd92a
Abdela umer <adheremo65@gmail.com>
2023-03-18 16:27:45 -0400

    string lab is done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// const currentUser = 'GRACE HOPPER';
+// let welcomeMessage = "Welcome to Flatbook, GRACE HOPPER !"
+
+// let excitedWelcomeMessage = "WELCOME TO FLATBOOK, GRACE HOPPER !";
+
+// let shortGreeting = "Welcome";
+
+const currentUser = 'Grace Hopper'
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const  shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
